86835151036d8d2e6d4b7a0b780131ecc6eac07b
wcypersonal <417177466@qq.com>
2018-09-18 16:21:13 +0800

    添加input问题的解决

--- src/App.js
+++ src/App.js
@@ -3,6 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            kindOfPage: 'foo',
+            reduxState: {
+                value: 20,
+                value1: 10
+            },
+        }
+    }
+
+    changeRedux() {
+        if (this.state.kindOfPage === 'foo') {
+            this.setState({
+                kindOfPage: 'bar'
+            })
+        } else {
+            this.setState({
+                kindOfPage: 'foo'
+            })
+        }
+        console.log(this.state.kindOfPage)
+        this.forceUpdate()
+    }
+
+    renderInput () {
+        var page = this.state.kindOfPage
+        var rState = this.state.reduxState;
+        if (page === 'foo') {
+            // foo input
+            // eg. rState.value === 10
+            return <input type="string" placeholder={rState.value1} key={"input1"}
+                          onChange={this.handleBar} />
+        } else {
+            // bar input
+            // eg. rState.value === 20
+            console.log('test')
+            return <input type="string" placeholder={rState.value} key={"input2"}
+                          onChange={this.handleFoo} />
+        }
+    }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +52,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <button onClick={this.changeRedux.bind(this)}>更换</button>
+          {this.renderInput()}
       </div>
     );
   }
